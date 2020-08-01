fd98ee939c42b9faaaccba876aa60ff3ec084894
xsv24 <thomaspearson.dev@gmail.com>
2020-08-01 12:21:21 +0100

    add layer update

--- keyboards/crkbd/lib/layer_state_reader.c
+++ keyboards/crkbd/lib/layer_state_reader.c
@@ -1,4 +1,3 @@
-
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 #include "crkbd.h"
@@ -10,27 +9,35 @@
 #define L_ADJUST 8
 #define L_ADJUST_TRI 14
 
+#define L_MAC 16
+#define L_MAC_LOWER 32
+#define L_MAC_RAISE 64
+#define L_MAC_ADJUST 224
+
 char layer_state_str[24];
+char os_state_str[9];
 
 const char *read_layer_state(void) {
-  switch (layer_state)
-  {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
-    break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
-    break;
-  default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
-  }
+    switch (layer_state) {
+        case L_BASE:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: ABC");
+            break;
+        case L_RAISE:
+        case L_MAC_RAISE:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: { } </>");
+            break;
+        case L_LOWER:
+        case L_MAC_LOWER:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: (8) & =>");
+            break;
+        case L_ADJUST:
+        case L_MAC_ADJUST:
+        case L_ADJUST_TRI:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: FUNC");
+            break;
+        default:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld ?", layer_state);
+    }
 
-  return layer_state_str;
+    return layer_state_str;
 }
